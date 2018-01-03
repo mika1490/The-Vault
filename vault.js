20406e334c217c6417abf0b1aeffcaa0f888da0b
Mika Dumont <mikakrantman@gmail.com>
2018-01-02 18:48:42 -1000

    finished vault

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+    var hi = {}
+    let setValue = function(key, value) {
+        hi[key] = value;
+    }
+
+       let getValue = function(key) { 
+           if(key === undefined || !hi[key])  {
+            return null;
+        } else {
+            return hi[key];
+        }
+       
+    }
+    return {
+        setValue: setValue,
+        getValue: getValue
+    }
+    
 };
